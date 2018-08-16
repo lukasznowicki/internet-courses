29b0e81c282d87dd6a32454767ec64e6cd6aaa78
lukasznowicki <py3z0CTuv>
2018-08-16 12:44:26 +0200

    Update .gitignore, create main plugin (along with directory structure) file with header

---
+++ wordpress/wp-content/plugins/internet-courses-by-phylax/internet-courses-by-phylax.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Internet Courses by phylax.pl
+Plugin URI: https://phylax.pl/
+Description: Create your internet courses as posts and easy navigation
+Version: 0.1.1
+Author: Łukasz Nowicki
+Author URI: https://lukasznowicki.info/
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: internet-courses-by-phylax
+Domain Path: /languages
+Requires at least: 4.9
+Tested up to: 4.9.8
+Requires PHP: 7.2
+Stable tag: 0.1
+
+Internet Courses by phylax.pl is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or any later
+version.
+
+Internet Courses by phylax.pl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+Internet Courses by phylax.pl. If not, see https://wordpress.org/about/license/.
+
+*/
